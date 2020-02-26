348d3a978903dc92e1afd42e56cd5ad73a78fd5b
Lionel Delamare <lionel.delamare@hotmail.com>
2020-02-26 09:34:55 +0100

    progression

--- modules/quotation/src/Controller/AdminQuotationController.php
+++ modules/quotation/src/Controller/AdminQuotationController.php
@@ -13,22 +13,26 @@ class AdminQuotationController extends FrameworkBundleAdminController
 {
     public function quotationIndex(Request $request)
     {
+
+
+
+
         //dump($this->get('quotation_repository')->findAll());die;
 
 
 
-        $form = $this->createForm(QuotationType::class, $quotation);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($quotation);
-            $entityManager->flush();
-        }
+//        $form = $this->createForm(QuotationType::class, $quotation);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $entityManager = $this->getDoctrine()->getManager();
+//            $entityManager->persist($quotation);
+//            $entityManager->flush();
+//        }
 
         return $this->render('@Modules/quotation/templates/admin/index_quotation.html.twig', [
             'quotations' => $quotations,
-            'form' => $form->createView()
+//            'form' => $form->createView()
         ]);
     }
 

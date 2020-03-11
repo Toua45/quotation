fd85acf7dd7d6bd6524dc61597f0651133449424
Lionel Delamare <lionel.delamare@hotmail.com>
2020-03-11 14:26:28 +0100

    module autocompletion

--- modules/quotation/assets/js/app.js
+++ modules/quotation/assets/js/app.js
@@ -1,52 +1,62 @@
-const DOM = {
-    currentElement: null,
-    urlCustomers: document.getElementById('customers').dataset.customers.replace(/\?(?=\d)(\w|\W)+/g, ''),
-    customers: null,
-};
+import {QuotationModule} from './customer';
+// import * as customer from './quotation';
+import {customer} from '../../../../adminLionel/data-customer';
 
-// var inputCustomer = document.getElementById('quotation_customerId');
-
-window.addEventListener('DOMContentLoaded', function (Event) {
-    var customerJson = document.getElementById('js-data');
-    var url = customerJson.dataset.source;
-    fetch(url).then(function (response) {return response.json();}).then(function (data) {
-        getCustomers();
-    })
-        .catch(function (error) {console.log(error);});
-});
-
-var substringMatcher = function (strs) {
-    return function findMatches(q, cb) {
-        var matches, substringRegex;
-        matches = [];
-        var substrRegex = new RegExp(q, 'i');
-        $.each(strs, function (i, str) {
-            if (substrRegex.test(str)) {
-                matches.push(str);
-            }
-        });
-        cb(matches);
-    };
-};
+// console.log(customer);
+QuotationModule.customerList();
+QuotationModule.customers(QuotationModule.customerList());
+QuotationModule.fetch(); // Appel de la fonction "customer"
 
 
-const autocompletition = function (customers) {
-    $('#quotation_customerId').typeahead({
-            hint: true,
-            highlight: true,
-            minLength: 1
-        },
-        {
-            name: 'customers',
-            source: substringMatcher(customers)
-        });
-};
-
-
-function getCustomers() {
-    fetch(DOM.urlCustomers).then(response => response.json()).then(function (data) {
-        DOM.customers = data;
-        autocompletition(data);
-    })
-        .catch(function (error) {console.log(error)});
-}
+// const DOM = {
+//     currentElement: null,
+//     urlCustomers: document.getElementById('customers').dataset.customers.replace(/\?(?=\d)(\w|\W)+/g, ''),
+//     customers: null,
+// };
+//
+// // var inputCustomer = document.getElementById('quotation_customerId');
+//
+// window.addEventListener('DOMContentLoaded', function (Event) {
+//     var customerJson = document.getElementById('js-data');
+//     var url = customerJson.dataset.source;
+//     fetch(url).then(function (response) {return response.json();}).then(function (data) {
+//         getCustomers();
+//     })
+//         .catch(function (error) {console.log(error);});
+// });
+//
+// var substringMatcher = function (strs) {
+//     return function findMatches(q, cb) {
+//         var matches, substringRegex;
+//         matches = [];
+//         var substrRegex = new RegExp(q, 'i');
+//         $.each(strs, function (i, str) {
+//             if (substrRegex.test(str)) {
+//                 matches.push(str);
+//             }
+//         });
+//         cb(matches);
+//     };
+// };
+//
+//
+// const autocompletition = function (customers) {
+//     $('#quotation_customerId').typeahead({
+//             hint: true,
+//             highlight: true,
+//             minLength: 1
+//         },
+//         {
+//             name: 'customers',
+//             source: substringMatcher(customers)
+//         });
+// };
+//
+//
+// function getCustomers() {
+//     fetch(DOM.urlCustomers).then(response => response.json()).then(function (data) {
+//         DOM.customers = data;
+//         autocompletition(data);
+//     })
+//         .catch(function (error) {console.log(error)});
+// }
--- modules/quotation/assets/js/customer.js
+++ modules/quotation/assets/js/customer.js
@@ -1,154 +1,65 @@
-// <<<<<<< HEAD
-// // export const CustomerModule = {
-// //     regex: /\d+(?=\/ajax)/,
-// //     DOM: {
-// //         placeholderClient: 'Sélectionnez le client',
-// //         urlCustomers: document.getElementById('customers').dataset.customers.replace(/[^(/adminLionel/data-customer.js)](\w|\W)+/g, '')
-// //     },
-// //
-// //     customerList: function () { return document.getElementById('quotation_customerId') },
-// //
-// //     // var customers = [];
-// // // var inputCustomer = document.getElementById('quotation_customerId');
-// // // console.log(inputCustomer);
-// // inputCustomer.addEventListener('keyup', function (Event) {
-// //     var customerJson = document.getElementById('js-data');
-// //     var url = customerJson.dataset.source;
-// //     fetch(url)
-// //         .then(function (response) {
-// //             return response.json();
-// //         })
-// //
-// //         .then(function (data) {
-// //             customers = data;
-// //             // autocompletion(dataCustomers.data);
-// //             CustomerModule.customersCall();
-// //             // console.log(dataCustomers.data)
-// //         })
-// //
-// //         .catch(function (error) {
-// //             console.log(error);
-// //         });
-// //
-// // });
-// //
-// // customersCall: function fetchCustomers() {
-// //     fetch(CustomerModule.DOM.urlCustomers).then(response => response.json()).then(customers => console.log(customers));
-// // };
-// //
-// //
-// // // console.log(dataCustomers);
-// // var substringMatcher = function (strs) {
-// =======
-// // import $ from 'jquery';
-// //
-// // export const QuotationCustomerModule = {
-// //     DOM: {
-// //         currentElement: null,
-// //         urlCustomers: document.getElementById('customers').dataset.customers.replace(/[^(/adminToua/data-customer.js)](\w|\W)+/g, '')
-// //     },
-// //
-// // inputCustomer: function () { return document.getElementById('quotation_customerId') },
-// //     customers: function (element) {
-// //         element.addEventListener('mousedown', function (Event) {
-// //             QuotationCustomerModule.DOM.currentElement = Event.currentTarget;
-// //
-// //     let customerJson = document.getElementById('js-data');
-// //     let url = customerJson.dataset.source;
-// //     fetch(url)
-// //         .then(function (response) {
-// //             return response.json();
-// //         })
-// //         .then(function (data) {
-// //             // console.log(data)
-// //         })
-// //         .catch(function (error) {
-// //             console.log(error);
-// //         });
-// // });
-// //
-// // customersCall: function fetchCustomers() {
-// //     fetch(QuotationModule.DOM.urlCustomers).then(response => response.json()).then(customers => console.log(customers));
-// // };
-// //
-// // substringMatcher: function (strs) {
-// >>>>>>> 3ce160e7929981a5ceeee3b271ac25340d9a9813
-// //     return function findMatches(q, cb) {
-// //         var matches, substringRegex;
-// //         // an array that will be populated with substring matches
-// //         matches = [];
-// //         // regex used to determine if a string contains the substring `q`
-// <<<<<<< HEAD
-// //         var substrRegex = new RegExp(q, 'i');
-// =======
-// //         let substrRegex = new RegExp(q, 'i');
-// >>>>>>> 3ce160e7929981a5ceeee3b271ac25340d9a9813
-// //         // iterate through the pool of strings and for any string that
-// //         // contains the substring `q`, add it to the `matches` array
-// //         $.each(strs, function (i, str) {
-// //             if (substrRegex.test(str)) {
-// //                 matches.push(str);
-// //             }
-// //         });
-// //         cb(matches);
-// //     };
-// <<<<<<< HEAD
-// // };
-// // // console.log(dataCustomers.data);
-// //
-// // function autocompletion(customers) {
-// //     $('#the-basics .linked-select').typeahead({
-// //             hint: true,
-// //             highlight: true,
-// //             minLength: 1
-// //         },
-// //         {
-// //             name: 'customers',
-// //             source: substringMatcher(customers)
-// //         });
-// // }
-// =======
-// // }
-// // ,
-// //
-// // $('#the-basics .linked-select').typeahead({
-// //         hint: true,
-// //         highlight: true,
-// //         minLength: 1
-// //     },
-// //     {
-// //         name: 'customers',
-// //         source: substringMatcher(data_test)
-// //     })
-// // }
-// // ;
-// //
-// // // var substringMatcher = function (strs) {
-// // //     return function findMatches(q, cb) {
-// // //         var matches, substringRegex;
-// // //         // an array that will be populated with substring matches
-// // //         matches = [];
-// // //         // regex used to determine if a string contains the substring `q`
-// // //         substrRegex = new RegExp(q, 'i');
-// // //         // iterate through the pool of strings and for any string that
-// // //         // contains the substring `q`, add it to the `matches` array
-// // //         $.each(strs, function (i, str) {
-// // //             if (substrRegex.test(str)) {
-// // //                 matches.push(str);
-// // //             }
-// // //         });
-// // //         cb(matches);
-// // //     };
-// // // };
-// // // var data_test = ["Anonymous Anonymous","John DOE","Lionel Delamare"];
-// // //
-// // // $('#the-basics .linked-select').typeahead({
-// // //         hint: true,
-// // //         highlight: true,
-// // //         minLength: 1
-// // //     },
-// // //     {
-// // //         name: 'customers',
-// // //         source: substringMatcher(data_test)
-// // //     });
-// >>>>>>> 3ce160e7929981a5ceeee3b271ac25340d9a9813
+export const QuotationModule = {
+    DOM: {
+        currentElement: null,
+        urlCustomers: document.getElementById('customers').dataset.customers.replace(/\?(?=\d)(\w|\W)+/g, ''),
+        customers: null,
+    },
+
+    customerList: function () {
+        return document.getElementById('quotation_customerId')
+    },
+
+    fetch: function () {
+         window.addEventListener('DOMContentLoaded', function (Event) {
+            QuotationModule.DOM.currentElement = Event.currentTarget;
+            let customerJson = document.getElementById('js-data');
+            let url = customerJson.dataset.source;
+
+            fetch(url).then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                QuotationModule.customers();
+            })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        });
+    },
+
+    substringMatcher: function (strs) {
+        return function findMatches(q, cb) {
+            let matches, substringRegex;
+            matches = [];
+            let substrRegex = new RegExp(q, 'i');
+            $.each(strs, function (i, str) {
+                if (substrRegex.test(str)) {
+                    matches.push(str);
+                }
+            });
+            cb(matches);
+        }
+    },
+
+    autocompletition: function (customers) {
+        $('#quotation_customerId').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1
+            },
+            {
+                name: 'customers',
+                source: QuotationModule.substringMatcher(customers)
+            })
+    },
+
+    customers: function () {
+        fetch(QuotationModule.DOM.urlCustomers).then(response => response.json()).then(function (data) {
+            console.log(data);
+            QuotationModule.DOM.customers = data;
+            QuotationModule.autocompletition(data);
+        })
+            .catch(function (error) {
+                console.log(error)
+            });
+    },
+};

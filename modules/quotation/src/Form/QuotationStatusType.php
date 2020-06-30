2d514a9e170f0c0ddf9cace2179f42bac4bc602f
Toua <toua.va45@gmail.com>
2020-06-30 17:23:40 +0200

    code about select quotation status realised

---
+++ modules/quotation/src/Form/QuotationStatusType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Quotation\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class QuotationStatusType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->setMethod('POST')
+            ->add('status', ChoiceType::class, [
+                'required' => false,
+                'label' => 'État du devis',
+                'placeholder' => 'Afficher tous les status',
+                'choices' => [
+                    'À valider' => 'validate',
+                    'Validé' => 'validated',
+                    'Approuvé' => 'approved',
+                    'Refusé' => 'refused'
+                ],
+                'attr' => [
+                    'class' => 'input-text'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
